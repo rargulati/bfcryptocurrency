d5d957b9fd526877e02f435fd8af28f66137fd24
Raghav Gulati <rargulati@users.noreply.github.com>
2018-02-18 09:32:26 -0800

    checkpoint

--- merkle/merkle.go
+++ merkle/merkle.go
@@ -20,23 +20,20 @@ type MerkleTree struct {
 	leafs      []*Node
 }
 
-func hashData(data ...[]byte) string {
-	h := sha256.New()
-	for _, d := range data {
-		_, _ = h.Write(d)
-	}
-	return fmt.Sprintf("%x", h.Sum(nil))
+func hashData(data string) string {
+	// h := sha256.New()
+	// _, _ = h.Write([]byte(data))
+	sum := sha256.Sum256([]byte(data))
+	return fmt.Sprintf("%x", sum)
 }
 
 func BuildSubTrees(leafs []*Node) (*Node, error) {
-	// if m == nil {
-	// 	return nil, fmt.Errorf("Cannot insert value into nil merkle tree")
-	// }
-	// for each leaf node (that's every two)
 	var nodes []*Node
+	// for each leaf node (that's every two)
 	for i := 0; i < len(leafs); i += 2 {
-		intermediate := append([]byte(leafs[i].hash), []byte(leafs[i+1].hash)...)
-		h := hashData([]byte(intermediate))
+		// intermediate := append([]byte(leafs[i].hash), []byte(leafs[i+1].hash)...)
+		intermediate := fmt.Sprintf("%s%s", leafs[i].hash, leafs[i+1].hash)
+		h := hashData(intermediate)
 		fmt.Printf("HASH OF intermediate(%d): %s\n", i, h)
 		n := &Node{
 			Left:  leafs[i],
@@ -47,12 +44,13 @@ func BuildSubTrees(leafs []*Node) (*Node, error) {
 		// set our parent node for our new sub-tree nodes
 		leafs[i].Parent = n
 		leafs[i+1].Parent = n
-		// If we only have two nodes left, we're the root
+		// We're working at the last layer, we're the root
 		if len(leafs) == 2 {
 			return n, nil
 		}
 	}
 	fmt.Println(len(nodes))
+	// recursively call this with our new layer
 	return BuildSubTrees(nodes)
 }
 
@@ -62,14 +60,14 @@ func NewMerkleTree(data []string) *MerkleTree {
 	for _, s := range data {
 		n := &Node{
 			data: s,
-			hash: hashData([]byte(s)),
+			hash: hashData(s),
 		}
 		m.leafs = append(m.leafs, n)
 	}
 	// odd case - where we have an odd number of leaves
-	// if len(m.leafs)%2 == 1 {
-	// 	m.leafs = append(m.leafs, &Node{data: "", hash: hashData([]byte(""))})
-	// }
+	if len(m.leafs)%2 == 1 {
+		m.leafs = append(m.leafs, &Node{data: "", hash: hashData("")})
+	}
 	return m
 }
 
