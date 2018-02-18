6fea41e44f9db7f87820c57635d3f32721053645
Raghav Gulati <rargulati@users.noreply.github.com>
2018-02-18 13:18:15 -0800

    complete merkle

--- merkle/merkle.go
+++ merkle/merkle.go
@@ -20,19 +20,43 @@ type MerkleTree struct {
 	leafs      []*Node
 }
 
+func NewMerkleTree(data []string) *MerkleTree {
+	m := &MerkleTree{}
+	// create and populate the leaf layer of the merkle tree
+	for _, s := range data {
+		// TODO: avoid pre-image attacks with padding our data
+		n := &Node{
+			data: s,
+			hash: hashData(s),
+		}
+		m.leafs = append(m.leafs, n)
+	}
+	// odd case - where we have an odd number of leaves
+	// pad with an empty string
+	if len(m.leafs)%2 == 1 {
+		m.leafs = append(m.leafs, &Node{data: "", hash: hashData("")})
+	}
+	// TODO: number of combatants is an even number
+	// if len(m.leafs/2)%2 == 1 {
+	// 	m.leafs = append(m.leafs, &Node{data: "", hash: hashData("")})
+	// 	m.leafs = append(m.leafs, &Node{data: "", hash: hashData("")})
+	// }
+	return m
+}
+
 func hashData(data string) string {
-	// h := sha256.New()
-	// _, _ = h.Write([]byte(data))
-	sum := sha256.Sum256([]byte(data))
-	return fmt.Sprintf("%x", sum)
+	h := sha256.New()
+	if _, err := h.Write([]byte(data)); err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
 
 func BuildSubTrees(leafs []*Node) (*Node, error) {
 	var nodes []*Node
 	// for each leaf node (that's every two)
 	for i := 0; i < len(leafs); i += 2 {
-		// intermediate := append([]byte(leafs[i].hash), []byte(leafs[i+1].hash)...)
-		intermediate := fmt.Sprintf("%s%s", leafs[i].hash, leafs[i+1].hash)
+		intermediate := fmt.Sprintf("%s||%s", leafs[i].hash, leafs[i+1].hash)
 		h := hashData(intermediate)
 		fmt.Printf("HASH of intermediate(%d): %s\n", i, h)
 		n := &Node{
@@ -54,31 +78,13 @@ func BuildSubTrees(leafs []*Node) (*Node, error) {
 	return BuildSubTrees(nodes)
 }
 
-func NewMerkleTree(data []string) *MerkleTree {
-	m := &MerkleTree{}
-	// create and populate the leaf layer of the merkle tree
-	for _, s := range data {
-		// TODO: avoid pre-image attacks with padding our data
-		n := &Node{
-			data: s,
-			hash: hashData(s),
-		}
-		m.leafs = append(m.leafs, n)
-	}
-	// odd case - where we have an odd number of leaves
-	// pad with an empty string
-	if len(m.leafs)%2 == 1 {
-		m.leafs = append(m.leafs, &Node{data: "", hash: hashData("")})
-	}
-	return m
-}
-
 func main() {
+	var err error
 	data := []string{"We", "hold", "these", "truths", "to", "be", "self-evident", "that"}
 	m := NewMerkleTree(data)
-	root, err := BuildSubTrees(m.leafs)
+	m.root, err = BuildSubTrees(m.leafs)
 	if err != nil {
 		fmt.Println("err: ", err)
 	}
-	fmt.Printf("ROOT: %s", root.hash)
+	fmt.Printf("ROOT: %s", m.root.hash)
 }
